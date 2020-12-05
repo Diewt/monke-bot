44a5a3b3ab90cbcb1b48063186c5e7e9199bd18f
stonesha <stoneshaus@gmail.com>
2020-12-04 21:19:10 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    num = random.randint(0, 10)
+    num = random.randint(0, 30)
 
     if message.author == client.user:
         return
@@ -19,6 +19,14 @@ async def on_message(message):
         await message.channel.send('me monke')
     elif num == 6:
         await message.channel.send('no care, me monke')
+    elif num == 7:
+        await message.add_reaction(emoji='monkey')
+    elif num == 8:
+        await message.add_reaction(emoji='monkey_face')
+    elif num == 9:
+        await message.channel.send(':monkey:')
+    elif num == 4:
+        await message.channel.send(':monkey_face:')
 
 
 client.run('Nzg0NjM5NzU3MzA0NzI1NTM0.X8sOuA.eyxrOiSH2kkjLEJFWoBPMoHKOOw')
