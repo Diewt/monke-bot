503b678a0c8b2c0c2f4e39660e47526f714dde00
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-05-23 00:50:29 -0700

    added animal image recognition

--- bot.py
+++ bot.py
@@ -2,11 +2,26 @@ import discord
 import random
 import os
 import time
+import requests
 
 from db import DB
 
+from torchvision import models, transforms
+import torch
+from PIL import Image
+
 client = discord.Client()
 db = DB()
+resnet = models.resnet101(pretrained=True)
+
+preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )])
 
 #read in file of possible messages
 with open("possible_messages.txt", "r") as files:
@@ -14,25 +29,48 @@ with open("possible_messages.txt", "r") as files:
 
 possible_messages = [i.strip('[]').replace('\n', '') for i in possible_messages]
 
+def recognize_image(url):
+    im = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+    img_t = preprocess(im)
+    batch_t = torch.unsqueeze(img_t, 0)
+    resnet.eval()
+    out = resnet(batch_t)
+    with open('imagenet_classes.txt') as f:
+        labels = [line.strip() for line in f.readlines()]
+
+    _, index = torch.max(out, 1)
+
+    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    animal = labels[index[0]].split(', ')[1]
+    animal = animal.replace("_", ' ')
+    im.close()
+
+    return str("Me am " + str(int(percentage[index[0]].item())) + "% sure me see " + str(animal))
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+
+    if message.author == client.user:
+        return
+
     num = random.randint(0, 100)
-    num2 = random.randint(0, 100000000)
 
     #check if user exists, otherwise insert into database
     matching_users = db.getMatchingUserId(message.author.id)
     if matching_users.shape[0] == 0:
         db.createNewUserEntry(message.author.id)
 
-    if message.author == client.user:
-        return
-
-    if num2 == 69:
-        await message.channel.send('https://giant.gfycat.com/SpottedUnconsciousGalapagostortoise.mp4')
+    if message.content.lower().endswith(('jpeg', 'png', 'jpg')):
+        recognize_str = recognize_image(message.content)
+        await message.channel.send(recognize_str)
+    elif message.attachments:
+        for a in message.attachments:   
+            recognize_str = recognize_image(a.url)
+            await message.channel.send(recognize_str)
 
     if num == 69:
         await message.channel.send(random.choice(possible_messages))
